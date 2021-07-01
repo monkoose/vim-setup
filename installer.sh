89481f99fcea6a8db08ee61c2a2baaa7cd102f22
monkoose <monkoose@protonmail.com>
2021-07-01 17:07:41 +0300

    Initial

---
+++ installer.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# Helper functions
+msg() {
+    printf "\033[1m%b\033[0m\n" "$1" >&2
+}
+
+error() {
+    msg "\33[31m[✘]\33[0m ${1}${2}"
+    exit 1
+}
+
+program_exists() {
+    command -v "$1" &>/dev/null || error "You must have '$1' installed to continue."
+}
+
+variable_set() {
+    [[ -n "$1" ]] || error "You must have your HOME environmental variable set to continue."
+}
+
+# Setup
+msg "Checking..."
+variable_set "$HOME"
+program_exists "curl"
+program_exists "git"
+program_exists "vim"
+install_dir="$HOME/.vim"
+repo="https://github.com/monkoose/vim-setup"
+vimplug_repo="https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+echo ""
+msg "Creating directories..."
+mkdir -pv "$install_dir/undo-files"
+echo ""
+msg "Cloning repo..."
+git clone --depth=1 "$repo" "$install_dir"
+echo ""
+msg "Begin fetching vim-plug..."
+curl -fLo "$install_dir/autoload/plug.vim" --create-dirs "$vimplug_repo"
+echo ""
+vim +PlugInstall
---
+++ vim9.vim
@@ -0,0 +1,24 @@
+vim9script
+
+if empty(prop_type_get('yank_prop'))
+  prop_type_add('yank_prop', { highlight: "HighlightedyankRegion", combine: true, priority: 100})
+endif
+
+def HighlightOnYank(timeout: number)
+  if v:event.operator == 'y' && !(empty(v:event.regtype))
+    const pos1 = getcharpos("'[")
+    const pos2 = getcharpos("']")
+    const start = [pos1[1], pos1[2] + pos1[3]]
+    const end = [pos2[1], pos2[2] + pos2[3]]
+
+    prop_add(start[0], start[1], { end_lnum: end[0], end_col: end[1] + 1, type: 'yank_prop' })
+    timer_start(timeout, (_) => prop_remove({ type: 'yank_prop' }, start[0], end[0]))
+  endif
+enddef
+
+augroup PostYank
+  autocmd!
+  autocmd TextYankPost * silent call HighlightOnYank(200)
+augroup END
+
+defcompile
